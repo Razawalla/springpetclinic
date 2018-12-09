e26fa0e062e6fb9e9563b7bcd6aa291d3af76841
Krishna <bkrishna9572@gmail.com>
2018-12-09 22:39:09 +0530

    Added Long ID. Closes #10.

--- pet-clinic-data/src/main/java/com/beekay/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/beekay/springpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.beekay.springpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
