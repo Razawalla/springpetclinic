63fc4523a9b3062ff4b22beadc42f15ba6bc0725
Krishna <bkrishna9572@gmail.com>
2018-12-09 23:33:03 +0530

    Added CrudService. Closes #11.

--- pet-clinic-data/src/main/java/com/beekay/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/beekay/springpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.beekay.springpetclinic.services;
 
 import com.beekay.springpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vpet);
-
-    Set<Vet> findAll();
 }
