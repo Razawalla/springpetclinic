6d921464267662516a2fdefa7744e0783246020e
Krishna <bkrishna9572@gmail.com>
2018-12-11 16:03:00 +0530

    Added ID Generation in AbstractMapService. Closes #23.

--- pet-clinic-web/src/main/java/com/beekay/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/beekay/springpetclinic/bootstrap/DataLoader.java
@@ -24,28 +24,24 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("bee");
         owner1.setLastName("kay");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Aarthi");
         owner2.setLastName("Gandhi");
 
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("NewFirst");
         vet1.setLastName("NewLast");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("NextFirst");
         vet2.setLastName("NextLast");
 
