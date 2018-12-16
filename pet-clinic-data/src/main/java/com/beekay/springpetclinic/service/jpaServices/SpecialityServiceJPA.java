c901ac1f7f321ffccc2fcb02ecc512b2bd9992e2
Krishna <bkrishna9572@gmail.com>
2018-12-17 00:28:57 +0530

    Implemented SpecialityServiceJPA. Closes #44

---
+++ pet-clinic-data/src/main/java/com/beekay/springpetclinic/service/jpaServices/SpecialityServiceJPA.java
@@ -0,0 +1,48 @@
+package com.beekay.springpetclinic.service.jpaServices;
+
+import com.beekay.springpetclinic.model.Speciality;
+import com.beekay.springpetclinic.repository.SpecialityRepository;
+import com.beekay.springpetclinic.service.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class SpecialityServiceJPA implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialityServiceJPA(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
