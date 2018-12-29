3a3c56e4f48f4fffe1458fb4f17e91e731b1ebe9
Krishna <bkrishna9572@gmail.com>
2018-12-29 17:26:26 +0530

    Displayed Owner Record. Closes #53.

--- pet-clinic-web/src/main/java/com/beekay/springpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/beekay/springpetclinic/controller/OwnerController.java
@@ -4,7 +4,10 @@ import com.beekay.springpetclinic.service.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -26,6 +29,13 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners(){
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView getOwnerById(@PathVariable Long ownerId){
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject("owner", ownerService.findById(ownerId));
+        return mav;
     }
 
 }
